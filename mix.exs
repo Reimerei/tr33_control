3d537f0e9386f66948b2be5ba299b6ad765086ea
Björn Reimer <git@bjrm.de>
2018-10-08 13:16:56 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Tr33Control.Mixfile do
       {:cowboy, "~> 1.0"},
       {:ecto_enum, "~> 1.0"},
       {:sqlite_ecto2, "~> 2.2"},
-      {:distillery, "~> 1.5", runtime: false}
+      {:distillery, "~> 2.0", runtime: false}
     ]
   end
 
