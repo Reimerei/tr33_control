29207b4ab6ee800e30966e58b34d251ff70083e8
reimerei <git@bjrm.de>
2021-05-23 20:30:34 +0200

    zw

--- lib/tr33_control/commands/command.ex
+++ lib/tr33_control/commands/command.ex
@@ -1,20 +1,32 @@
 defmodule Tr33Control.Commands.Command do
   use Ecto.Schema
 
+  alias Tr33Control.Commands
+
   @primary_key false
   embedded_schema do
     # internal
     field :index, :integer, default: 0
     field :enabled, :boolean, default: true
-    field :targets, {:array, Tr33Control.Atom}, default: Application.fetch_env!(:tr33_control, :command_targets)
+    field :targets, {:array, Tr33Control.Atom}, default: Application.compile_env!(:tr33_control, :command_targets)
 
     # protobuf
-    field :message, :map
+    field :params, :map
     field :encoded, :binary
   end
 
-  def encode(%__MODULE__{message: %{__struct__: type} = msg} = command) do
+  def encode(%__MODULE__{params: %{__struct__: type} = msg} = command) do
     encoded = apply(type, :encode, [msg])
     %__MODULE__{command | encoded: encoded}
+  end
+
+  def list_params(%__MODULE__{params: %{__struct__: type} = params}) do
+    params
+    |> Map.from_struct()
+    |> Map.drop([:common])
+    |> Map.keys()
+    |> Enum.map(&apply(type, :defs, [:field, &1]))
+    |> Enum.reject(&is_nil/1)
+    |> Enum.sort_by(& &1.fnum)
   end
 end
